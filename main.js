bde55eadfefe4a08e072f4fff08913b14fa0cbce
Braian Fernandes <braian.afm@gmail.com>
2023-02-25 17:48:22 -0300

    novos arquivos

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('form').on('submit', function(e) {
+        e.preventDefault(); 
+        const novaTarefa = $('#tarefa').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`
+            <div>
+                <a>${novaTarefa}</a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#tarefa').val('');
+    })
+
+})
